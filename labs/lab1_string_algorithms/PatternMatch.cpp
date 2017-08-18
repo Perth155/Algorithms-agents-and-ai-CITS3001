32f2cb99fbca8a45160665e6d4b9ee7832323455
Perth155 <abrar.a.amin@gmail.com>
2017-08-19 00:15:10 +0800

    lab1 initial commit

---
+++ labs/lab1_string_algorithms/PatternMatch.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+/**
+ * 
+ *
+ */
+int naivePatternMatch(string smallStr, string bigStr)
+{
+
+	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	string bigStr;
+	string smallStr; 
+	cout << "Enter big str (text): ";
+	getline(cin, bigStr);
+	cout << "Enter small str (pattern): ";
+	getline(cin, smallStr);	
+	cout << bigStr << "   " <<smallStr <<endl;
+	exit(0);
+}
