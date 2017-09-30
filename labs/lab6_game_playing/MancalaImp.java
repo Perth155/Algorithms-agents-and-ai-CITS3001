152969e8df7f2cf0605315cfda0ad4de427f04c9
Perth155 <abrar.a.amin@gmail.com>
2017-09-30 14:03:06 +0800

    checking win condition

--- labs/lab6_game_playing/MancalaImp.java
+++ labs/lab6_game_playing/MancalaImp.java
@@ -5,6 +5,58 @@
 public class MancalaImp implements MancalaAgent
 {
 
+	private int getGameWinner(int[] b)
+	{
+		if(b[6] > b[13]) {return 1;}
+		else if(b[13] > b[6]) {return 2;}
+		else {return 0;}
+	}
+
+	/**
+	 * A method that looks to see if the game is currently over, if so return an integer that
+	 * represents the winner.
+	 * @param gameBoard showing the current state of the game.
+	 * @return -1 if game is Active, 1 if player1 (this agent) wins, 2 if player2 (opponent) wins
+	 * and 0 if match is tied. 
+	 */
+	private int checkForWin(int[] gameBoard)
+	{
+		int gameResult = -1;
+		boolean gameOver = true;
+		//Check if player1 ended the game by exhausting his/her houses.
+		for(int i = 0; i < 6; i++)
+		{
+			gameOver = (gameOver && (gameBoard[i] == 0));
+		}
+		if(gameOver)
+		{
+			for(int j = 7; j < 13; j++)
+			{
+				gameBoard[13]+=gameBoard[j];
+				gameBoard[j] = 0;
+			}
+			gameResult = getGameWinner(gameBoard);	
+			return gameResult;
+		}
+		//check if player2 ended the game by exhausting his/her houses.
+		gameOver = true;
+		for(int j = 7; j < 13; j++)
+		{
+			gameOver = (gameOver && (gameBoard[j] == 0));
+		}
+		if(gameOver)
+		{
+			for(int i = 0; i < 6; i++)
+			{
+				gameBoard[6]+=gameBoard[i];
+				gameBoard[i] = 0;
+			}
+			gameResult = getGameWinner(gameBoard);
+			return gameResult;
+		}
+		return gameResult;
+	}
+
 	 /**
  	* Allows the agent to nominate the house the agent would like to move seeds from. 
  	* The agent will allways have control of houses 0-5 with store at 6. 
@@ -22,8 +74,11 @@ public class MancalaImp implements MancalaAgent
  	*/
  	public int move(int[] board)
  	{
+		//int winner = checkForWin(int[] board);
+			//if(gameOver() != -1)
+			//{}
 		 return 0;
-	 }
+	}
 
 
 	/**
@@ -32,7 +87,7 @@ public class MancalaImp implements MancalaAgent
 	*/
 	public String name()
 	{
-		  return "HAL";
+		  return "HAL by abraram";
 	}
 
 	/**
